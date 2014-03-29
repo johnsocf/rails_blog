5b24ce7bdf2d67ce9f83be54cb47c26931cdd6ad
Catherine Johnson <cfjohnson@earthlink.net>
2014-03-29 13:38:51 -0600

    built initial structure of rails blog.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBlog::Application.config.secret_key_base = '49c78a8d8c2a8a212d39f8f37058f9edf0f44fe929f515e7a64180b53d52c60fd59ad3ffa22321a04a93836fc6ffcb77f0126628b9f03a728925b2c6b1b9edf8'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+RailsBlog::Application.routes.draw do
+  root 'home#index'
+  #go to home controller and index action
+end
