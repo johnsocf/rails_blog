2cf9d19a0df548f498322bb6eff0abfd9ac37d20
Catherine Johnson <cfjohnson@earthlink.net>
2014-03-29 20:29:26 -0600

    updated pathing for posts pages form and controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,15 @@
 class PostsController < ApplicationController
+	def new
+	end
+
+	def create
+		redirect_to posts_path
+		#todo rails generate model post
+		# rails generate scaffold post - analyze what's generated.
+		Post.create
+	end
+
 	def index
 	end
+
 end
